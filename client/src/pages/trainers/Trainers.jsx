05be106661cac5bc9d149296f4e9c8459c5c25a5
Phong Nguyễn <nguyenthanhphongkg0209@gmail.com>
2024-02-04 13:51:19 +0700

    trainers

--- client/src/pages/trainers/Trainers.jsx
+++ client/src/pages/trainers/Trainers.jsx
@@ -9,8 +9,9 @@ import { collection, getDocs } from "firebase/firestore";
 import SectionHead from "../../components/SectionHead";
 import Card from "../../components/Card";
 import { Link } from "react-router-dom";
-import { Button, Dropdown, Menu, Row, Space } from "antd";
+import { Button, Dropdown, Menu, Row, Space, Pagination } from "antd";
 import { useAuth } from "../../context/AuthContext";
+import loadingGIF from "../../images/loading.gif"
 
 
 import ModalCustom from "../../components/ModalCustom";
@@ -59,29 +60,41 @@ const Trainers = () => {
   const { TrainersAll } = useAuth(); 
   const [filter, setFilter] = useState("all");
   const [selectedMenuItem, setSelectedMenuItem] = useState(null);
+  const [currentPage, setCurrentPage] = useState(3);
+const [pageSize, setPageSize] = useState(6);
+const [loading, setLoading] = useState(false);
 
-    async function fetchData() {
-
-    try {
-        const trainers = await TrainersAll();
-        setUsers(trainers);
-        setSortedList(trainers);
-    } catch (error) {
-        console.error("Error fetching trainers:", error);
-    }
+async function fetchData() {
+  try {
+    const trainers = await TrainersAll();
+    setUsers(trainers);
+    setSortedList(trainers);
+  } catch (error) {
+    console.error("Error fetching trainers:", error);
+  } finally {
+    setLoading(false); // Kết thúc việc tải dữ liệu, đặt loading thành false
+  }
 }
+
   
 
 useEffect(() => {
+  setLoading(true);
   fetchData();
 }, [])
-if (!sortedList) {
-  return <h1>Loading...</h1>
+if (loading) {
+  return <div className="w-40 mt-44 mx-auto" >
+            <img src={loadingGIF} alt="Loading" />
+        </div>
 }
 
+
   const handleRatingClick = ({ key }) => {
+
     sortRating(key);
     setSelectedMenuItem(key);
+
+    
   };
   const rating = (
     <Menu onClick={handleRatingClick}>
@@ -135,26 +148,53 @@ if (!sortedList) {
     5: { min: 8001, max: 10000 }, 
   }; 
   const getAll = () => {
+    setLoading(true)
     setFilter("all");
-    setSortedList(users);
+    setTimeout(() =>{
+      setSortedList(users);
+      setLoading(false); 
+    },1000);
+   
   };
 
   const sortRating = (rating) => {
+    setLoading(true);
     setFilter("rating");
-    setSortedList(users.filter((user) => user.rating === rating));
+    setTimeout(() => {
+      setSortedList(users.filter((user) => user.rating === rating));
+      setLoading(false); 
+  }, 1000); 
   };
 
   const sortField = (field) => {
+    setLoading(true); 
     setFilter("field");
-    setSortedList(users.filter((user) => user.field === field));
+    setTimeout(() => {
+    setSortedList(users.filter((user) => user.field === field)); 
+    setLoading(false);
+  }, 1000);
   };
 
   const sortPrice = (minPrice, maxPrice) => {
+    setLoading(true); 
     setFilter("price");
+    setTimeout(() => {
     setSortedList(
       users.filter((user) => user.price >= minPrice && user.price <= maxPrice)
+
     );
+    setLoading(false); 
+  }, 1000);
   };
+
+  const paginate = (items, currentPage, pageSize) => {
+    if (!items) return [];
+    const startIndex = (currentPage - 1) * pageSize;
+    return items.slice(startIndex, startIndex + pageSize);
+    
+  };
+
+  const paginatedList = paginate(sortedList, currentPage, pageSize);
 
   return (
     <>
@@ -165,78 +205,85 @@ if (!sortedList) {
       <section className="trainers">
         <div className="container">
           <SectionHead icon={<FaCrown />} title="Trainers" />
-          <Space></Space>
+          <Space style={{ margin: '20px 0' }}></Space>
 
-          <Row style={{ justifyContent: "space-evenly", width: "50%" }}>
+          <Row style={{ justifyContent: "space-evenly", width: "50%", margin: '30px 0' }}>
           <Space></Space>
-          <Button type="primary" style={{background: filter === "all" ? "#0080ff" : "inherit"}} onClick={() => getAll()}>All</Button>
+          <Button type="primary" style={{background: filter === "all" ? "#0080ff" :"#4b5c988f"}} onClick={() => getAll()}>All</Button>
           <Space></Space>
           <Dropdown overlay={rating} placement="bottomLeft">
-            <Button type="primary" style={{background: filter === "rating" ? "#0080ff" : "inherit"}}>Rating</Button>
+            <Button type="primary" style={{background: filter === "rating" ? "#0080ff" : "#4b5c988f"}}>Rating</Button>
           </Dropdown>
           <Dropdown overlay={field} placement="bottomLeft">
-            <Button type="primary" style={{background: filter === "field" ? "#0080ff" : "inherit"}}>Field</Button>
+            <Button type="primary" style={{background: filter === "field" ? "#0080ff" : "#4b5c988f"}}>Field</Button>
           </Dropdown>
           <Space></Space>
           <Dropdown overlay={price} placement="bottomLeft">
-            <Button type="primary" style={{background: filter === "price" ? "#0080ff" : "inherit"}}>Price</Button>
+            <Button type="primary" style={{background: filter === "price" ? "#0080ff" : "#4b5c988f"}}>Price</Button>
           </Dropdown>
 
         </Row>
-          <div className="trainers">
-            <div className="container grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 gap-16">
-            {sortedList &&
-              sortedList.length > 0 &&
-              sortedList.map(
-                (trainer) => {
-                  const { id, photoURL, displayName, price, field, rating } = trainer;
-                    return (
-                      <Card className="" key={id}>
-                        <div onClick={() => {setOpen(true);setTrainerInfor(trainer);console.log(trainer)}} className="relative group mb-5 transition-all duration-200 hover:saturate-100 cursor-pointer">
-                          <div className="w-full h-60 bg-contain">
-                            <img
-                              src={photoURL}
-                              className="rounded-3xl w-full h-full"
-                              style={{ objectPosition: "10px -20px" }}
-                              alt="trainer one"
-                            />
-                          </div>
-                          <div className="opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full "></div>
-                          <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center opacity-0 hover:opacity-100 ">
-                            <h1 className="text-lg">View information</h1>
-                          </div>
-                        </div>
-                        <h3 className="text-[20px] text-[#EAB308] font-semibold">
-                          {displayName}
-                        </h3>
-                        <p>{field}</p>
-                        <div className="my-3 flex justify-evenly mx-auto items-center">
-                          <h1>{`$ ${price}`}</h1>
-                          <select
-                            name=""
-                            id=""
-                            className="text-black px-1 rounded-md"
-                          >
-                            <option value="">1 month</option>
-                            <option value="">2 month</option>
-                            <option value="">3 month</option>
-                          </select>
-                        </div>
-                        <div className="flex justify-center mb-5">
-                          {rating > 0 &&
-                            Array(Math.round(rating)).fill(
-                              <AiFillStar color="yellow" />
-                            )}
-                        </div>
-                        <Link to={"plans"} className="btn sm">
-                          Booking
-                        </Link>
-                      </Card>
-                    );
-                  }
-                )}
-            </div>
-          </div>
+        <div className="trainers">
+        <div className="container grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 gap-16">
+          {paginatedList && paginatedList.length > 0 ? (
+            paginatedList.map((trainer) => (
+              <Card className="" key={trainer.id}>
+                <div
+                  onClick={() => {
+                    setOpen(true);
+                    setTrainerInfor(trainer);
+                    console.log(trainer);
+                  }}
+                  className="relative group mb-5 transition-all duration-200 hover:saturate-100 cursor-pointer"
+                >
+                  <div className="w-full h-60 bg-contain">
+                    <img
+                      src={trainer.photoURL}
+                      className="rounded-3xl w-full h-full"
+                      style={{ objectPosition: "10px -20px" }}
+                      alt="trainer one"
+                    />
+                  </div>
+                  <div className="opacity-0 bg-gradient-to-t from-gray-800 via-gray-800 to-opacity-30 group-hover:opacity-50 absolute top-0 left-0 h-full w-full "></div>
+                  <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center opacity-0 hover:opacity-100 ">
+                    <h1 className="text-lg">View information</h1>
+                  </div>
+                </div>
+                <h3 className="text-[20px] text-[#EAB308] font-semibold">
+                  {trainer.displayName}
+                </h3>
+                <p>{trainer.field}</p>
+                <div className="my-3 flex justify-evenly mx-auto items-center">
+                  <h1>{`$ ${trainer.price}`}</h1>
+                  <select name="" id="" className="text-black px-1 rounded-md">
+                    <option value="">1 month</option>
+                    <option value="">2 month</option>
+                    <option value="">3 month</option>
+                  </select>
+                </div>
+                <div className="flex justify-center mb-5">
+                  {trainer.rating > 0 &&
+                    Array(Math.round(trainer.rating)).fill(
+                      <AiFillStar color="yellow" />
+                    )}
+                </div>
+                <Link to={"plans"} className="btn sm">
+                  Booking
+                </Link>
+              </Card>
+            ))
+          ) : (
+            <div className="text-center text-red-500">No coach information found</div>
+          )}
+        
+        </div>
+      </div>
+      <Pagination style={{margin: '30px 0',  display: 'flex', justifyContent: 'center',}}
+      current={currentPage}
+      pageSize={pageSize}
+      total={sortedList.length}
+      onChange={(page, pageSize) => setCurrentPage(page)}
+    />
         </div>
         <TrainerModal open={open} setOpen={setOpen} trainer={trainerInfor} />
       </section>
